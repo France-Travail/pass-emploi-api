9ee7dff13111642907aa25a014203f23d49e58bb
marie-bisault <65164427+marie-bisault@users.noreply.github.com>
2025-12-23 15:57:02 +0100

    refactor: migration api get utilisateurs
    Co-authored-by: brib-octo <brice.boutamdja@octo.com>

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -76,7 +76,7 @@ export default () => {
         process.env.MILO_SESSIONS_DETAIL_ET_LISTE_JEUNE_API_KEY,
       apiKeySessionsListeConseiller:
         process.env.MILO_SESSIONS_LISTE_CONSEILLER_API_KEY,
-      apiKeyUtilisateurs: process.env.MILO_UTILISATEURS_API_KEY,
+      apiKeyUtilisateurs: process.env.MILO_V2_UTILISATEURS_API_KEY,
       apiKeyEnvoiEmail: process.env.MILO_ENVOI_EMAIL_API_KEY,
       urlWeb: process.env.MILO_WEB_URL
     },
